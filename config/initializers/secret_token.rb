0c41191031ea11d00b8df91b9c53b30a498f0ee5
pepin <pepin@MacBook-Pro-de-pepin.local>
2013-02-03 19:44:26 +0000

    Hello World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Printer::Application.config.secret_token = '3f3b25949d05238f28532c4ad3277b259bb03f51958c5b901c54e15f53596acd3bdf1571d5da4132901a09ddf080d4aedca81bf2f404c91cc7ad5966605f70f9'
